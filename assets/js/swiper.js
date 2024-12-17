eb9cb4216f8a38d54f40e322cd49559deabfbb31
huzaifa311 <huzaifahabib311@gmail.com>
2024-12-17 20:43:41 +0500

    Book Cover Design page Added

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -94,3 +94,27 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".mySwiper6", {
+        slidesPerView: 4,
+        spaceBetween: 6,
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+          320: { slidesPerView: 1, spaceBetween: 20 },
+          640: { slidesPerView: 2, spaceBetween: 3 },
+          1024: { slidesPerView: 4, spaceBetween: 5 },
+        },
+    });
+});
